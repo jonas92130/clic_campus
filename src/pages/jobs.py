5d7dbd959866a3d6c09d3fa4e8930a5ea9390e18
Jonas Bouaziz <jonas.bouaziz@epita.fr>
2024-06-15 16:22:56 +0200

    add secteurActivite

--- src/pages/jobs.py
+++ src/pages/jobs.py
@@ -14,11 +14,19 @@ def departement():
     return scrapper.query(endpoint="/referentiel/departements").json()
 
 
+@st.cache_data
+def secteur_activite():
+    return scrapper.query(endpoint="/referentiel/secteursActivites").json()
+
+
 with st.sidebar:
     with st.form("run", border=False):
         deps = departement()
         departement = st.selectbox("Département", [d["code"] for d in deps],
                                    format_func=lambda x: [d["libelle"] for d in deps if d["code"] == x][0])
+        sects = secteur_activite()
+        secteur_activite = st.selectbox("Secteur d'activité", [d["code"] for d in sects],
+                                        format_func=lambda x: [d["libelle"] for d in sects if d["code"] == x][0])
         limit = st.slider("Limit", 1, 10000, 10)
         collection = st.text_input("Collection")
         st.write("Or select an existing collection")
@@ -27,7 +35,7 @@ with st.sidebar:
         if st.form_submit_button("Run"):
             st.session_state["job_collection"] = job_collection
             scrapper.db = Db.get_db("jobs", job_collection)
-            scrapper.run(parameters={"departement": departement}, limit=limit)
+            scrapper.run(parameters={"departement": departement, "secteurActivite": secteur_activite}, limit=limit)
 
 if job_collection := st.session_state.get("job_collection"):
     for doc in Db.get_documents("jobs", [job_collection]):
